bc75fb073790c3af3de5abb575874d173e1b02d9
Andrew Monteith <339531824@GoTVDSB.ca>
2023-10-18 10:56:04 -0400

    need to show them working

--- Program.cs
+++ Program.cs
@@ -10,7 +10,76 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            
+            while (true)
+            {
+                string menuOption;
+                Console.WriteLine("1. Get Hypotenuse 2. Get Leg 3. Get Rise 4. Get Run 5. Get Distance");
+                Console.WriteLine("6. Get Slope 7. Get Y-Intercept 8. Is Right? 9. Get Random List 10. Get Random Array");
+                Console.WriteLine("11. Reverse Word 12. Rootchecker 13. Circumference 14. Triangle Area 15. Parallelogram Area");
+                Console.WriteLine("Press Q to Quit");
+                menuOption = Console.ReadLine();
+                if (menuOption == "1")
+                {
+                    
+                }
+                else if (menuOption == "2")
+                {
+                    
+                }
+                else if (menuOption == "3")
+                {
+                    
+                }
+                else if (menuOption == "4")
+                {
+                    
+                }
+                else if (menuOption == "5")
+                {
+                    
+                }
+                else if (menuOption == "6")
+                {
+                    
+                }
+                else if (menuOption == "7")
+                {
+                    
+                }
+                else if (menuOption == "8")
+                {
+                    
+                }
+                else if (menuOption == "9")
+                {
+                    
+                }
+                else if (menuOption == "10")
+                {
+                    
+                }
+                else if (menuOption == "11")
+                {
+                    
+                }
+                else if (menuOption == "12")
+                {
+                    
+                }
+                else if (menuOption == "13")
+                {
+                    
+                }
+                else if (menuOption == "Q" || menuOption == "q")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
         }
+    }
     }
 }
